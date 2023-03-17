46a33390fe648b76dc888b0cdb6a0ce8afd4d55e
Matthieu Bucchianeri <69611560+mbucchia@users.noreply.github.com>
2023-03-17 00:58:04 -0700

    Initial commit

---
+++ XR_APILAYER_NOVENDOR_template/layer.cpp
@@ -0,0 +1,204 @@
+// MIT License
+//
+// Copyright(c) 2022 Matthieu Bucchianeri
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this softwareand associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright noticeand this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "pch.h"
+
+#include "layer.h"
+#include <log.h>
+#include <util.h>
+
+namespace {
+
+    using namespace openxr_api_layer;
+    using namespace openxr_api_layer::log;
+
+    class OpenXrLayer : public openxr_api_layer::OpenXrApi {
+      public:
+        OpenXrLayer() = default;
+
+        // Corresponds to xrDestroyInstance().
+        // https://www.khronos.org/registry/OpenXR/specs/1.0/html/xrspec.html#xrCreateInstance
+        ~OpenXrLayer() = default;
+
+        // https://www.khronos.org/registry/OpenXR/specs/1.0/html/xrspec.html#xrGetInstanceProcAddr
+        XrResult xrGetInstanceProcAddr(XrInstance instance, const char* name, PFN_xrVoidFunction* function) override {
+            TraceLoggingWrite(g_traceProvider,
+                              "xrGetInstanceProcAddr",
+                              TLXArg(instance, "Instance"),
+                              TLArg(name, "Name"),
+                              TLArg(m_bypassApiLayer, "Bypass"));
+
+            XrResult result = m_bypassApiLayer ? m_xrGetInstanceProcAddr(instance, name, function)
+                                               : OpenXrApi::xrGetInstanceProcAddr(instance, name, function);
+
+            TraceLoggingWrite(g_traceProvider, "xrGetInstanceProcAddr", TLPArg(*function, "Function"));
+
+            return result;
+        }
+
+        // https://www.khronos.org/registry/OpenXR/specs/1.0/html/xrspec.html#xrCreateInstance
+        XrResult xrCreateInstance(const XrInstanceCreateInfo* createInfo) override {
+            if (createInfo->type != XR_TYPE_INSTANCE_CREATE_INFO) {
+                return XR_ERROR_VALIDATION_FAILURE;
+            }
+
+            TraceLoggingWrite(g_traceProvider,
+                              "xrCreateInstance",
+                              TLArg(xr::ToString(createInfo->applicationInfo.apiVersion).c_str(), "ApiVersion"),
+                              TLArg(createInfo->applicationInfo.applicationName, "ApplicationName"),
+                              TLArg(createInfo->applicationInfo.applicationVersion, "ApplicationVersion"),
+                              TLArg(createInfo->applicationInfo.engineName, "EngineName"),
+                              TLArg(createInfo->applicationInfo.engineVersion, "EngineVersion"),
+                              TLArg(createInfo->createFlags, "CreateFlags"));
+            Log(fmt::format("Application: {}\n", createInfo->applicationInfo.applicationName));
+
+            // Here there can be rules to disable the API layer entirely (based on applicationName for example).
+            // m_bypassApiLayer = ...
+
+            if (m_bypassApiLayer) {
+                Log(fmt::format("{} layer will be bypassed\n", LayerName));
+                return XR_SUCCESS;
+            }
+
+            for (uint32_t i = 0; i < createInfo->enabledApiLayerCount; i++) {
+                TraceLoggingWrite(
+                    g_traceProvider, "xrCreateInstance", TLArg(createInfo->enabledApiLayerNames[i], "ApiLayerName"));
+            }
+            for (uint32_t i = 0; i < createInfo->enabledExtensionCount; i++) {
+                TraceLoggingWrite(
+                    g_traceProvider, "xrCreateInstance", TLArg(createInfo->enabledExtensionNames[i], "ExtensionName"));
+            }
+
+            // Needed to resolve the requested function pointers.
+            OpenXrApi::xrCreateInstance(createInfo);
+
+            // Dump the application name and OpenXR runtime information to help debugging issues.
+            XrInstanceProperties instanceProperties = {XR_TYPE_INSTANCE_PROPERTIES};
+            CHECK_XRCMD(OpenXrApi::xrGetInstanceProperties(GetXrInstance(), &instanceProperties));
+            const auto runtimeName = fmt::format("{} {}.{}.{}",
+                                                 instanceProperties.runtimeName,
+                                                 XR_VERSION_MAJOR(instanceProperties.runtimeVersion),
+                                                 XR_VERSION_MINOR(instanceProperties.runtimeVersion),
+                                                 XR_VERSION_PATCH(instanceProperties.runtimeVersion));
+            TraceLoggingWrite(g_traceProvider, "xrCreateInstance", TLArg(runtimeName.c_str(), "RuntimeName"));
+            Log(fmt::format("Using OpenXR runtime: {}\n", runtimeName));
+
+            return XR_SUCCESS;
+        }
+
+        // https://www.khronos.org/registry/OpenXR/specs/1.0/html/xrspec.html#xrGetSystem
+        XrResult xrGetSystem(XrInstance instance, const XrSystemGetInfo* getInfo, XrSystemId* systemId) override {
+            if (getInfo->type != XR_TYPE_SYSTEM_GET_INFO) {
+                return XR_ERROR_VALIDATION_FAILURE;
+            }
+
+            TraceLoggingWrite(g_traceProvider,
+                              "xrGetSystem",
+                              TLXArg(instance, "Instance"),
+                              TLArg(xr::ToCString(getInfo->formFactor), "FormFactor"));
+
+            const XrResult result = OpenXrApi::xrGetSystem(instance, getInfo, systemId);
+            if (XR_SUCCEEDED(result) && getInfo->formFactor == XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY) {
+                if (*systemId != m_systemId) {
+                    XrSystemProperties systemProperties{XR_TYPE_SYSTEM_PROPERTIES};
+                    CHECK_XRCMD(OpenXrApi::xrGetSystemProperties(instance, *systemId, &systemProperties));
+                    TraceLoggingWrite(g_traceProvider, "xrGetSystem", TLArg(systemProperties.systemName, "SystemName"));
+                    Log(fmt::format("Using OpenXR system: {}\n", systemProperties.systemName));
+                }
+
+                // Remember the XrSystemId to use.
+                m_systemId = *systemId;
+            }
+
+            TraceLoggingWrite(g_traceProvider, "xrGetSystem", TLArg((int)*systemId, "SystemId"));
+
+            return result;
+        }
+
+        // https://www.khronos.org/registry/OpenXR/specs/1.0/html/xrspec.html#xrCreateSession
+        XrResult xrCreateSession(XrInstance instance,
+                                 const XrSessionCreateInfo* createInfo,
+                                 XrSession* session) override {
+            if (createInfo->type != XR_TYPE_SESSION_CREATE_INFO) {
+                return XR_ERROR_VALIDATION_FAILURE;
+            }
+
+            TraceLoggingWrite(g_traceProvider,
+                              "xrCreateSession",
+                              TLXArg(instance, "Instance"),
+                              TLArg((int)createInfo->systemId, "SystemId"),
+                              TLArg(createInfo->createFlags, "CreateFlags"));
+
+            const XrResult result = OpenXrApi::xrCreateSession(instance, createInfo, session);
+            if (XR_SUCCEEDED(result)) {
+                if (isSystemHandled(createInfo->systemId)) {
+                }
+
+                TraceLoggingWrite(g_traceProvider, "xrCreateSession", TLXArg(*session, "Session"));
+            }
+
+            return result;
+        }
+
+      private:
+        bool isSystemHandled(XrSystemId systemId) const {
+            return systemId == m_systemId;
+        }
+
+        bool m_bypassApiLayer{false};
+        XrSystemId m_systemId{XR_NULL_SYSTEM_ID};
+    };
+
+    std::unique_ptr<OpenXrLayer> g_instance = nullptr;
+
+} // namespace
+
+namespace openxr_api_layer {
+    OpenXrApi* GetInstance() {
+        if (!g_instance) {
+            g_instance = std::make_unique<OpenXrLayer>();
+        }
+        return g_instance.get();
+    }
+
+    void ResetInstance() {
+        g_instance.reset();
+    }
+
+} // namespace openxr_api_layer
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        TraceLoggingRegister(openxr_api_layer::log::g_traceProvider);
+        break;
+
+    case DLL_PROCESS_DETACH:
+        TraceLoggingUnregister(openxr_api_layer::log::g_traceProvider);
+        break;
+
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}
