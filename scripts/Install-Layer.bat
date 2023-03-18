dcb032f2cdabc2383cebb553404554ae67a8408b
Matthieu Bucchianeri <mbucchia@gmail.com>
2023-03-17 23:09:09 -0700

    Remote VCRT dependency and turn the powershell scripts into batch.

---
+++ scripts/Install-Layer.bat
@@ -0,0 +1,2 @@
+reg add HKLM\Software\Khronos\OpenXR\1\ApiLayers\Implicit /v "%~dp0\XR_APILAYER_MBUCCHIA_varjo_foveated.json" /t REG_DWORD /d 0x00000000 /f
+@pause
---
+++ scripts/Uninstall-Layer.bat
@@ -0,0 +1,1 @@
+reg delete HKLM\Software\Khronos\OpenXR\1\ApiLayers\Implicit /v %~dp0\XR_APILAYER_MBUCCHIA_varjo_foveated.json /f
